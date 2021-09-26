51a945200cfc0705b9a4af91e9836927ef9fc2b1
鄭嘉雯 <betty60226aa@gmail.com>
2021-09-26 17:20:04 +0800

    no message

---
+++ js/api.js
@@ -0,0 +1,20 @@
+const api_url = "https://in-food.herokuapp.com/";
+
+
+function getRestaurants(){
+    const req = new Request(api_url+"api/v1/restaurant");
+    fetch(req, {
+        method: 'GET',
+        credentials: 'include', 
+        mode: 'cors',
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Credentials": true,
+        },
+      }).then(res =>  res.json())
+      .then((data) => {console.log(data)})
+      .catch((err) => {console.log(err)})
+};
+
+
